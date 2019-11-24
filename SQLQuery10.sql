188557b3c2580aa9ec309b4d31e21a1064106523
Dmitrii Saburov <Saburov.98@mail.ru>
2019-11-24 19:31:43 +0300

    wow

--- SQLQuery10.sql
+++ SQLQuery10.sql
@@ -1,3 +1,4 @@
+--use ProductsAndCategories;
 --create table Products
 --(
 --Id int IDENTITY PRIMARY KEY,
@@ -11,8 +12,8 @@
 --create table ProductCategory
 --(
 --ProductId int NOT NULL,
---CategoryId int NULL,
---PRIMARY KEY(ProductId)
+--CategoryId int not NULL,
+--PRIMARY KEY(ProductId, CategoryId)
 --)
 --use ProductsAndCategories;
 --insert into Products values
@@ -32,23 +33,23 @@
 --('овощи'),
 --('фрукты')
 --use ProductsAndCategories;
+--use ProductsAndCategories;
 --insert into ProductCategory(ProductId, CategoryId) values
---(1,3),
---(2,3),
---(3,3),
---(4,2),
---(5,2),
---(6,1),
---(7,1),
---(8,1),
---(9,1),
+--(1,2),
+--(2,2),
+--(3,2),
+--(4,1),
+--(5,3),
+--(6,3),
+--(7,3),
+--(8,3),
+--(9,3)
 --(10, null)
-use ProductsAndCategories;
+
 select 
 (select ProductName from Products where Id = ProductCategory.ProductId) as Product,
 (select CategoryName from Categories where Id = ProductCategory.CategoryId) as Category 
 from ProductCategory
-
 
 
 
